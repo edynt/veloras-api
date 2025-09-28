4ac58472995ac7f22cb0680839b672a589e6576e
Tri Nguyen <triminh.nguyen2098@gmail.com>
2025-09-28 16:19:17 +0700

    refresh token

--- internal/auth/application/service/auth.service.go
+++ internal/auth/application/service/auth.service.go
@@ -10,5 +10,5 @@ type AuthService interface {
 	CreateUser(ctx context.Context, accountAppDTO appDto.AccountAppDTO) (int, error)
 	VerifyUser(ctx context.Context, verificationEmailAppDTO appDto.EmailVerification) (bool, error)
 	LoginUser(ctx context.Context, accountAppDTO appDto.AccountAppDTO) (appDto.UserOutPut, error)
-    RefreshToken(ctx context.Context, refreshToken string) (appDto.TokenOut, error)
+	RefreshToken(ctx context.Context, refreshToken string) (appDto.TokenOut, error)
 }
